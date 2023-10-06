8e978bd6635a55ce044432873f7b6a14bb9343d8
Pedro.Piassi <pedro.a.piassi@gmail.com>
2023-10-05 22:52:02 -0300

    feat: criando a parte de categorias

--- config/routes.rb
+++ config/routes.rb
@@ -4,9 +4,10 @@ Rails.application.routes.draw do
 
   # Defines the root path route ("/")
   # root "articles#index"
-  root to: "home#index"
+  root to: 'home#index'
 
   namespace :admin do
-    root to: "home#index"
+    root to: 'home#index'
+    resources :categories
   end
 end
