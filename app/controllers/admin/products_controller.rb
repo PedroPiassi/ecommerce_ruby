555f7fd16fe415a9998273341d617ea240b689cf
Pedro.Piassi <pedro.a.piassi@gmail.com>
2023-10-07 12:16:35 -0300

    feat: crud de produtos

---
+++ app/controllers/admin/products_controller.rb
@@ -0,0 +1,57 @@
+module Admin
+    class ProductsController < BaseController
+      before_action :set_products, only: %i[ show edit update destroy ]
+
+        def index
+            @products = Product.all
+        end
+
+        def edit; end
+        
+        def new
+            @product = Product.new
+        end
+
+        def show; end
+
+        def create
+            @product = Product.new(product_params)
+
+            respond_to do |format|
+                if @product.save
+                    format.html { redirect_to admin_products_path(@product), notice: 'Produto criado com sucesso.' }
+                else
+                    format.html { render :new, status: :unprocessable_entity }
+                end
+            end
+        end
+
+        def update
+            respond_to do |format|
+                if @product.update(product_params)
+                    format.html { redirect_to admin_products_path(@product), notice: 'Produto alterado com sucesso.' }
+                else
+                    format.html { render :edit, status: :unprocessable_entity }
+                end
+            end
+        end
+
+        def destroy
+            @product.destroy
+  
+            respond_to do |format|
+              format.html { redirect_to admin_products_path, notice: "Produto deletada com sucesso." }
+            end
+        end
+
+        def product_params
+            params.require(:product).permit(:name, :description, :price, :publish, :image, :category_id)
+        end
+
+        private
+
+        def set_products
+            @product = Product.find(params[:id])
+        end
+    end
+end
