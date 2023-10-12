bb0cf847f63204d2030011c39b7f2afe3e6f1ef6
Pedro.Piassi <pedro.a.piassi@gmail.com>
2023-10-11 23:13:49 -0300

    feat: update de dados do usuario

--- app/controllers/settings_controller.rb
+++ app/controllers/settings_controller.rb
@@ -1,5 +1,17 @@
 class SettingsController < ApplicationController
   before_action :authenticate_user!
-  def index
+  
+  def index; end
+
+  def update_user
+    if current_user.update(user_params)
+      redirect_to settings_path, notice: 'Dados atualizados com sucesso.'
+    else
+      render :index
+    end
+  end
+
+  def user_params
+    params.require(:user).permit(:name, :phone)
   end
 end
