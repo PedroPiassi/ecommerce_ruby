b67450891ea72cca775361a30f93787cc8f6b276
Pedro.Piassi <pedro.a.piassi@gmail.com>
2023-11-26 12:29:53 -0300

    feat: alterando menu e alterar senha

--- app/controllers/settings_controller.rb
+++ app/controllers/settings_controller.rb
@@ -3,6 +3,8 @@ class SettingsController < ApplicationController
   
   def index; end
 
+  def password; end
+
   def update_user
     if current_user.update(user_params)
       redirect_to settings_path, notice: 'Dados atualizados com sucesso.'
@@ -11,7 +13,30 @@ class SettingsController < ApplicationController
     end
   end
 
+  def update_password
+    if !current_password_params[:current_password].empty? && current_user.valid_password?(current_password_params[:current_password])
+      if current_user.update(password_params)
+        bypass_sign_in(current_user)
+        redirect_to password_settings_path, notice: 'Senha atualizada com sucesso.'
+      else
+        render :password
+      end
+    else
+      redirect_to password_settings_path, notice: 'Senha atual inválida.'
+    end
+  end
+
+  private
+
   def user_params
     params.require(:user).permit(:name, :phone, :avatar)
+  end
+
+  def current_password_params
+    params.require(:user).permit(:current_password)
+  end
+
+  def password_params
+    params.require(:user).permit(:password, :password_confirmation)
   end
 end
